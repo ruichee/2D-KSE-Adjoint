127b8e7dfb5ab84a41a0863e0c1c8b08fa530173
ruichee <maoruijie@yahoo.com>
2026-01-09 22:42:51 +0800

    Implemented get_R()

--- 1D_KS.py
+++ 1D_KS.py
@@ -3,12 +3,17 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
-def get_R(u, f):
-    pass
+def get_R(u, f, kx):
+    u_sq = u**2
+    u_sqf = np.fft.fft(u_sq**2)
+    u_sqf_x = np.complex128(kx) * u_sqf
+    u_sq_x = np.fft.irfft(u_sqf_x)
+    R = 0.5 * u_sq_x
+    return R
 
 
-def get_G(u, f):
-    R = get_R(u, f)
+def get_G(u, f, kx):
+    R = get_R(u, f, kx)
     pass
 
 
@@ -44,19 +49,23 @@ def ngh_descent(u, f, dt, n_iter, tol):
     return u
 
 
-
 def plot_data():
     pass
 
 
-def main(u0, L, f, dt, n_iter_adj, n_iter_ngh, tol_adj, tol_ngh):
+def main(u0, L, n, f, dt, n_iter_adj, n_iter_ngh, tol_adj, tol_ngh):
+
+    # given n = EVEN number of collocation points, define grid
+    x = np.linspace(0, L, n)
+    # fourier wave numbers (k) for DFT
+    kx = 2*np.pi/L * np.arange(-n//2, n//2, 1)
     
 
     u = adj_descent(u0, f, dt, n_iter_adj, tol_adj)
     
     # check if ngh descent is required here
 
-    u = ngh_descent(u0, f, dt, n_iter_ngh, tol_ngh)
+    u = ngh_descent(u, f, dt, n_iter_ngh, tol_ngh)
 
     plot_data()
 
