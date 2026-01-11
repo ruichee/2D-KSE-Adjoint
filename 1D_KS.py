f9705e2067cc239f730ce64d093b58a86e921cc9
ruichee <maoruijie@yahoo.com>
2026-01-11 21:42:27 +0800

    Added comments for get_R() steps

--- 1D_KS.py
+++ 1D_KS.py
@@ -6,53 +6,51 @@ from tqdm import tqdm
 def get_vars(domain_size, num_colloc_pts):
 
     L, n = domain_size, num_colloc_pts
-
-    # define spatial step
-    dx = L/n 
-
-    # given n = EVEN number of collocation points, define grid
-    x = np.linspace(0, L-dx, n)
-
-    # fourier wave numbers (k) for DFT
-    kx = 2*np.pi * np.fft.fftfreq(n, d=L/n)
-
-    return (x, kx)
+    dx = L/n                                    # define spatial step
+    x = np.linspace(0, L-dx, n)                 # n = EVEN no. of collocation points, define grid
+    kx = 2*np.pi * np.fft.fftfreq(n, d=L/n)     # fourier wave numbers (k) for DFT
+    return (x, kx)                              # NOTE: L-dx ensure no cutting into next period
 
 
 def dealiase(ff, kx):
+
     k = np.absolute(kx)
-    k_max = 1/3 * np.max(k)
-    ff_filtered = np.where(k < k_max, ff, 0)
+    k_max = 1/3 * np.max(k)                     # maximum frequency that we will keep
+    ff_filtered = np.where(k < k_max, ff, 0)    # all higher frequencies are set to 0
     return ff_filtered
 
 
 def get_R(u, f, kx): # TRY IMPLEMENTING VIA FINITE DIFFERENCE, VALIDATE IF FEASIBLE
 
     # non-linear term u∂ₓu in fourier space
-    u_sq = u**2
-    u_sqf = np.fft.fft(u_sq)
-    u_sqf_x = 1j * kx * u_sqf, kx
-    u_sqf_x = dealiase(u_sqf_x)
-    u_sq_x = np.fft.ifft(u_sqf_x)
-    udu = -0.5 * u_sq_x
+    u_sq = u**2                                 # obtain u^2, since u∂ₓu = 0/5*∂ₓ(u^2)
+    u_sqf = np.fft.fft(u_sq)                    # bring u^2 into fourier space
+    u_sqf_x = 1j * kx * u_sqf                   # multiply by ik to each u_k (differentiate in fourier)
+    u_sq_x = np.fft.ifft(u_sqf_x)               # convert back to physical space, we get ∂ₓ(u^2)
+    udu = -0.5 * u_sq_x                         # multiply by minus half to obtain -u∂ₓu
 
-    # add linear terms in fourier space
-    udu_f = np.fft.fft(udu)
-    u_f = np.fft.fft(u)
-    u_f = dealiase(u_f, kx)
-    R_f = udu_f + (kx**2 - kx**4)*u_f
-    R_f = dealiase(R_f, kx)
+    # add linear terms in fourier space -∂ₓₓu-∂ₓₓₓₓu
+    udu_f = np.fft.fft(udu)                     # bring u∂ₓu back to fourier
+    u_f = np.fft.fft(u)                         # bring u into fourier
+    u_f = dealiase(u_f, kx)                     # dealise u
+    R_f = udu_f + (kx**2 - kx**4)*u_f           # add linear terms, n-derivative = multiply u by (ik)^n
+    R_f = dealiase(R_f, kx)                     # dealise R
     
     # set mean flow = 0, i.e. wave has no DC component (constant offset)
-    R_f = np.where(kx == 0, 0, R_f)
+    R_f = np.where(kx == 0, 0, R_f)             # ensures the sine wave has no constant component (k=0)
 
     # convert back to physical space
-    R = np.real(np.fft.ifft(R_f))
+    R = np.real(np.fft.ifft(R_f))               # obtain R(u) = -u∂ₓu - ∂ₓₓu - ∂ₓₓₓₓu
     return R
 
 
 def get_G(u, f, kx):
+
+    # first obtain R
     R = get_R(u, f, kx)
+
+
+
     pass
 
 
@@ -66,7 +64,7 @@ def adj_descent(u, f, dt, n_iter, tol):
         u = un + dt*G # can implement rk45 later on
         
         err: float # implement error calc
-        if err < tol:
+        if err < tol: 
             break
 
     return u
@@ -94,7 +92,7 @@ def plot_data():
 
 def main(u0, L, n, f, dt, n_iter_adj, n_iter_ngh, tol_adj, tol_ngh):
 
-    x, kx = get_vars(domain_size=L, num_colloc_pts=n)
+    
 
     u = adj_descent(u0, f, dt, n_iter_adj, tol_adj)
     
@@ -105,13 +103,16 @@ def main(u0, L, n, f, dt, n_iter_adj, n_iter_ngh, tol_adj, tol_ngh):
     plot_data()
 
 
-L = 22
-n = 256
-dx = L/n
-x = np.linspace(0, L-dx, n) # the minus dx is important to maintain periodicity
-m=2
-u0=2*np.sin(m*2*np.pi*x/L)
-kx = 2*np.pi * np.fft.fftfreq(n, d=L/n)
+# define variables 
+L = 22      # domain size
+n = 128     # number of collocation points
+
+# obtain domain field (x), and fourier wave numbers kx
+x, kx = get_vars(domain_size=L, num_colloc_pts=n)
+
+# define initial conditions of field variable u
+m = 2
+u0 = 2*np.sin(m*2*np.pi*x/L)
 
 u = get_R(u0, 0, kx)
 plt.plot(u)
