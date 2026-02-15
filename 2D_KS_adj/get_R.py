a8929ced57833128ef9c40d4af47aae2f388d956
ruichee <maoruijie@yahoo.com>
2026-02-15 18:54:31 +0800

    real() on nonlin terms before squaring

--- 2D_KS_adj/get_R.py
+++ 2D_KS_adj/get_R.py
@@ -11,8 +11,8 @@ def get_R(t, u: np.ndarray[tuple[int, int], float], print_res=False) -> np.ndarr
     # non-linear term -1/2(∂ₓu)^2 in fourier space 
     u_x_f = 1j * KX * u_f                       # ∂ₓu in fourier, differentiate via multiply ik_x
     u_y_f = 1j * KY * u_f                       # ∂ᵧu in fourier, differentiate via multiply ik_y
-    u_x = np.fft.ifft2(u_x_f)                   # bring back to physical space
-    u_y = np.fft.ifft2(u_y_f)                   # bring back to physical space
+    u_x = np.real(np.fft.ifft2(u_x_f))                   # bring back to physical space
+    u_y = np.real(np.fft.ifft2(u_y_f))                   # bring back to physical space
     u_sq_terms = -0.5 * (u_x*u_x + u_y*u_y)     # get -1/2(∂ₓu)^2
 
     # linear terms -∂ₓₓu-∂ᵧᵧu-∂ₓₓₓₓu-∂ᵧᵧᵧᵧu-2∂ₓₓ∂ᵧᵧu in fourier space 
